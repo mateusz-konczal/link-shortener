4bd89a2fd3bce0091a3849f90861e01597f5d4fd
Mateusz Kończal <mateuszkonczal96@gmail.com>
2022-07-10 01:49:51 +0200

    Add @Controller annotation

--- src/main/java/pl/sii/linkshortener/controller/WebController.java
+++ src/main/java/pl/sii/linkshortener/controller/WebController.java
@@ -1,7 +1,9 @@
 package pl.sii.linkshortener.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class WebController {
     @RequestMapping("/")
     String indexHtml() {
